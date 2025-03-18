988e7bd70212e5c335d3d84170c3d386f1e16086
saraNasser4 <saransser6@gmail.com>
2025-03-18 02:06:05 +0200

    final touch for CustomizeProduct

--- app/components/CustomizeProduct.tsx
+++ app/components/CustomizeProduct.tsx
@@ -30,39 +30,31 @@ export default function CustomizeProduct({ productOptions }: CustomizeProductPro
     if(choice.inStock) setChosenSize(ind) 
   }
 
-  // console.log(productOptions)
-
   return (
     <section className="my-4">
       {productOptions.map((options)=> {
-        
         return (
-          <div key={options.name}>
+          <div key={options.name.toLowerCase()}>
             <h4 className="font-medium text-[18px] md:text-xl mb-2">Choose a {options.name}</h4>
             <ul className="flex items-center gap-4 my-4">
-              {options?.choices.map((choice, i)=> {   
-                console.log(choice.description)           
+              {options?.choices.map((choice, i)=> {          
                 return(
-                  <>
+                  <div key={choice.description}>
                     {options.name === "Color" && 
-                      <li key={choice.description} onClick={() => chooseColorHandler(choice, i)} className={`relative rounded-full w-8 h-8 ${choice.inStock ? 'cursor-pointer' : 'cursor-not-allowed'}`} style={{ backgroundColor: choice.value }}>
+                      <li  onClick={() => chooseColorHandler(choice, i)} className={`relative rounded-full w-8 h-8 ${choice.inStock ? 'cursor-pointer' : 'cursor-not-allowed'}`} style={{ backgroundColor: choice.value }}>
                         {chosenColor === i && <span className={`absolute w-10 h-10 ring-2 ring-zinc-500 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`}></span>}
                         {!choice.inStock && <span className={`absolute w-[2px] h-10 ring-2 bg-red-50 rounded-full top-1/2 left-1/2 -rotate-45 -translate-x-1/2 -translate-y-1/2`}></span>}
                       </li>
                     }
                     {options.name === "Size" && 
-                      <li key={choice.value} onClick={() => chooseSizeHandler(choice, i)} className={`relative rounded-xl ${choice.inStock ? 'cursor-pointer' : 'cursor-not-allowed'}`}>
+                      <li onClick={() => chooseSizeHandler(choice, i)} className={`relative rounded-xl ${choice.inStock ? 'cursor-pointer' : 'cursor-not-allowed'}`}>
                         <span className={`text-[17px] py-2 px-4 rounded-xl border ${choice.inStock && chosenSize === i ? 'border-primary text-primary' : !choice.inStock ? 'bg-light-blue' : 'bg-primary border-primary'}`}>{choice.value}</span>
                       </li>
                     }
-                  </>
+                  </div>
                 )
               }
               )}
-              {/* <li  */}
-                  {/* {chosenColor === ind && <span className={`absolute w-10 h-10 ring-2 ring-zinc-500 rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`}></span>} */}
-                  {/* {!color.available && <span className={`absolute bg-red-100 w-[2px] h-10 rounded-full top-1/2 left-1/2 -rotate-45 -translate-x-1/2 -translate-y-1/2`}></span>} */}
-              {/* </li> */}
             </ul>
           </div>
         )
