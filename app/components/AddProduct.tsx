75047c64871c5666088a80305cac03ab87e296fa
saraNasser4 <saransser6@gmail.com>
2025-04-16 10:24:20 +0200

    making add to cart btn dynamic

--- app/components/AddProduct.tsx
+++ app/components/AddProduct.tsx
@@ -1,28 +1,14 @@
 "use client"
 import { useEffect, useState } from 'react'
 import { FaMinus, FaPlus } from 'react-icons/fa6'
-import useWixClient from '../hooks/useWixClient'
+
+import AddBtn from './AddBtn'
 
 export default function AddProduct({ productId, productQuantity, variantId }: { productId: string, productQuantity: number, variantId?: string }) {
   const [quantity, setQuantity] = useState(1)
   const total = productQuantity || 0;
 
-  const wixClient = useWixClient()
-
-  const addItem = async () => {
-    const response = await wixClient.currentCart.addToCurrentCart({
-      lineItems:[
-        {
-          catalogReference: { 
-            appId: process.env.NEXT_PUBLIC_WIX_APP_ID,
-            catalogItemId: productId,
-            ...[variantId && { option: { variantId }}],
-          },
-          quantity: productQuantity,
-        }
-      ]
-    })
-  }
+  
   useEffect(()=> {
     if(total < 1) {
       setQuantity(0)
@@ -32,7 +18,7 @@ export default function AddProduct({ productId, productQuantity, variantId }: { 
   return (
     <section className='my-4'>
       <h4 className="font-medium text-[18px] md:text-xl mb-2">Choose Quantity</h4>
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-start flex-col lg:flex-row gap-4'>
         <div className='flex gap-4 items-center'>
           <div className='flex gap-4 items-center px-4 py-2 rounded-xl bg-zinc-800'>
             <button onClick={()=> setQuantity(prev => prev > 1 ? prev - 1 : 1)}><FaMinus /></button>
@@ -44,7 +30,7 @@ export default function AddProduct({ productId, productQuantity, variantId }: { 
             <p>Only <span className='text-primary'>{total} {total > 1 ? 'items' : 'item'}</span> left!<br /> Don&apos;t miss it</p>
           }
         </div>
-        <button onClick={()=> addItem()} className='text-sm rounded-xl px-4 py-2 ring-1 ring-primary text-primary transition-all duration-200 hover:text-white hover:bg-primary disabled:cursor-not-allowed disabled:text-white disabled:bg-light-blue'>Add to Cart</button>
+        <AddBtn productId={productId} productQuantity={productQuantity} variantId={variantId} />
       </div>
     </section>
   )
--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import wixServer from "../lib/wixServer";
 import Pagination from "./Pagination";
+import AddBtn from "./AddBtn";
 
 const PRODUCT_PER_PAGE: number = 6
 
@@ -37,37 +38,47 @@ export default async function Product({ categoryId, limit, searchParams, paginat
     }
     
     const itemsList = response?.items || []
+    console.log(itemsList[0])
+    
     
     return (
         <div className="flex gap-x-8 gap-y-16 justifybetween flex-wrap mt-10">
 
             {itemsList.length > 0 ?
                 <>
-                    {itemsList?.map(item =>  
-                        <Link key={item.name} href={`/${item.slug}`} className="w-full flex flex-col gap-4 sm:w-[46%] lg:w-[22%]">
-                            <div className="relative w-full h-80">
-                                <Image 
-                                    src={item.media?.items?.[0]?.image?.url || 'https://placehold.co/600x400'} 
-                                    alt={item.media?.items?.[0]?.title || 'product image'}
-                                    fill 
-                                    sizes="250vw" 
-                                    className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500"
-                                    />
-                                <Image 
-                                    src={item.media?.items?.[1]?.image?.url || 'https://placehold.co/600x400'} 
-                                    alt={item.media?.items?.[1]?.title || 'product image'}
-                                    fill 
-                                    sizes="250vw" 
-                                />
+                    {itemsList?.map(item => {
+                        const productId = item?.inventoryItemId
+                        return(
+
+                            <div key={item.name} className="w-full sm:w-[46%] lg:w-[22%] [&>:last-child]:my-5">
+                                <Link href={`/${item.slug}`} className="flex flex-col gap-4">
+                                    <div className="relative w-full h-80">
+                                        <Image 
+                                            src={item.media?.items?.[0]?.image?.url || 'https://placehold.co/600x400'} 
+                                            alt={item.media?.items?.[0]?.title || 'product image'}
+                                            fill 
+                                            sizes="250vw" 
+                                            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity duration-500"
+                                            />
+                                        <Image 
+                                            src={item.media?.items?.[1]?.image?.url || 'https://placehold.co/600x400'} 
+                                            alt={item.media?.items?.[1]?.title || 'product image'}
+                                            fill 
+                                            sizes="250vw" 
+                                        />
+                                    </div>
+                                    <div className="flex justify-between sm:text-xl">
+                                        <h4>{item.name}</h4>
+                                        <span>{item?.price?.price} {item?.price?.currency}</span>
+                                    </div>
+                                    <p className="line-clamp-1 text-[15px] text-gray-400">{item.description}</p>
+                                </Link>
+                                
+                                <AddBtn productId={productId} productQuantity={item?.productOptions?.[0]?.choices?.[0]?.inStock ? 1 : 0} variantId={"variantId"} />
                             </div>
-                            <div className="flex justify-between sm:text-xl">
-                                <h4>{item.name}</h4>
-                                <span>{item?.price?.price} {item?.price?.currency}</span>
-                            </div>
-                            <p className="line-clamp-1 text-[15px] text-gray-400">{item.description}</p>
-                            <button className="text-sm text-primary border border-primary w-max py-2 px-3 rounded-2xl hover:bg-primary hover:text-white">Add to Cart</button>
-                        </Link>
-                    )}
+                        )
+                         
+                    })}
                     
                     {paginationAppear && <Pagination currentPage={response?.currentPage || 0} hasPrev={response?.hasPrev()} hasNext={response?.hasNext()} />}
                 </>
