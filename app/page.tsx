2f52fd9c93982235b00a66083baf42d24084fe00
saraNasser4 <saransser6@gmail.com>
2025-04-17 21:14:46 +0200

    install redux tools

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,23 @@ import { Suspense } from "react";
 import Categories from "./components/Categories";
 import ProductList from "./components/ProductList";
 import Slider from "./components/Slider";
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 export default async function Home() {
   const categoryId = process.env.FEATURED_PRODUCTS_CATEGORY_ID || ""
   
   return (
     <main>
-      <Slider />
-      <Suspense fallback="loading...">
-        <ProductList categoryId= {categoryId} limit={4} />
-      </Suspense>
-      <Suspense fallback="loading...">
-        <Categories />
-      </Suspense>
+      <Provider store={store}>
+        <Slider />
+        <Suspense fallback="loading...">
+          <ProductList categoryId= {categoryId} limit={4} />
+        </Suspense>
+        <Suspense fallback="loading...">
+          <Categories />
+        </Suspense>
+      </Provider>
     </main>
   );
 }
